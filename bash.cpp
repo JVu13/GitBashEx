8cc6614a8f92134763a76169a5aae1404cc94917
John Vu <vujohn58@gmail.com>
2018-04-12 18:24:54 -0400

    Changes made

--- bash.cpp
+++ bash.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Messing with GitBash" << std::endl
+	int userInput = 0;
+	
+	std::cout << "Messing with GitBash" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Enter a number: " << std::endl;
+	std::cin >> userInput;
+	
+	std::cout << "Number is: " << userInput << std::end;
 }
